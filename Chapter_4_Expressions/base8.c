d7822a8c36623e945753a7b9f19b47ee1500c57a
Nathan <nburns9922@gmail.com>
2020-09-07 20:50:37 -0400

    Add half of chapter 4 challenges

---
+++ Chapter_4_Expressions/base8.c
@@ -0,0 +1,25 @@
+//Chapter 4 Programming Project #4
+//Write a program that reads an integer entered by the user
+//and displays it in octal (base 8)
+//I know there is a better way to do this with a loop, but at this point 
+//in the book we havent learned of any loops or decision statements
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Enter a number between 0 and 32767: ");
+    scanf("%ddd",&n);
+    int n1,n2,n3,n4,n5;
+    n1 = n % 8;
+    n = n / 8;
+    n2 = n % 8;
+    n = n / 8;
+    n3 = n % 8;
+    n = n / 8;
+    n4 = n % 8;
+    n = n / 8;
+    n5 = n % 8;
+    
+    printf("%d%d%d%d%d",n5,n4,n3,n2,n1);
+}
---
+++ Chapter_4_Expressions/reverse_digi.c
@@ -0,0 +1,30 @@
+//Chapter 4 Programming project #1,2,3 from King's Modern C
+//#1: Write a program that asks the user to enter a two
+//digit number then prints it reversed
+//#2: Make it handle 3 digit numbers
+//3: Make it not use arithmetic to split into digits
+#include <stdio.h>
+
+int main(void)
+{
+
+    /** first method
+    int digit;
+    printf("Enter a two-digit number: ");
+    scanf("%d",&digit);
+    int first = digit / 10;
+    int second = digit % 10;
+    printf("The reversal is: %d%d",second,first);
+    **/
+    /** second problem
+    int digit, middle;
+    printf("Enter a three-digit number: ");
+    scanf("%d",&digit);
+    printf("The reversal is: %d%d%d", digit %10, (digit /10)%10, digit /100);
+    **/
+    //third problem
+    int d1,d2,d3;
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d",&d1,&d2,&d3);
+    printf("%1d%1d%1d",d3,d2,d1);
+}
